3b70cd2de01e670afc55f92d91cc0bed2a4921c5
Alex Krivolutsky <irramail@gmail.com>
2020-01-28 18:36:16 +0700

    rewrite to fetch uploaded svg to redis

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,48 @@
 extern crate redis;
 
-use redis::{Commands};
-use jsonrpc_http_server::jsonrpc_core::{IoHandler, Value};
+use redis::{Client, Commands, Connection, RedisResult};
+use jsonrpc_http_server::jsonrpc_core::{IoHandler, Value, Params, Error};
 use jsonrpc_http_server::{ServerBuilder};
+use std::time::{SystemTime};
 
-fn get_data() -> redis::RedisResult<String> {
-  let client = redis::Client::open("redis://192.168.9.9/")?;
+fn parse_arguments (p: Params) -> Result<Vec<String>, Error> {
+  let mut result = Vec::new();
+  match p {
+    Params::Array(array) => {
+      for s in &array {
+        match s {
+          Value::String(s) => result.push(s.clone()),
+          _ => return Err(Error::invalid_params("expecting strings"))
+        }
+      }
+    }
+    _ => return Err(Error::invalid_params("expecting an array of strings"))
+  }
+  if result.len() < 1 {
+    return Err(Error::invalid_params("missing api key"));
+  }
+
+  return Ok(result[0..].to_vec());
+}
+
+fn fetch_svg(svg: &str) -> redis::RedisResult<isize> {
+  let client = redis::Client::open("redis://127.0.0.1/")?;
   let mut con = client.get_connection()?;
+  let svg = format!("{}", svg);
 
-  con.get("all")
+  let _ : () = con.set("svg", svg)?;
+
+  con.get("svg")
 }
 
 fn main() {
   let mut io = IoHandler::new();
 
-  io.add_method("get_data",  | _params | {
-    let data = get_data().unwrap();
-    Ok(Value::String((format!("[{{\"t\":0, \"a\":\"0\", \"c\":0}}, {} ,{{\"t\":0, \"a\":\"0\", \"c\":0}}]", &data[..data.len()-1])).to_string()))
+  io.add_method("get_data",  move |params: Params| {
+    let w = parse_arguments(params)?;
+    let _ = fetch_svg( &w[0]);
+
+    Ok(Value::String("".to_string()))
   });
 
   let server = ServerBuilder::new(io)
